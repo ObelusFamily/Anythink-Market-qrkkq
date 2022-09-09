52fcf66b710753da2d691db32e5c2ddaddfeb5a4
Avi Dalal <avi413@gmail.com>
2022-09-09 18:06:26 +0300

    bugfix: add validation to post  item

--- backend/models/Item.js
+++ backend/models/Item.js
@@ -8,7 +8,19 @@ var ItemSchema = new mongoose.Schema(
     slug: { type: String, lowercase: true, unique: true },
     title: String,
     description: String,
-    image: String,
+    image: {
+      type: String,
+      required: true,
+      validate: {
+        // describe the validate feature
+        validator(value) {
+          // validator is a data validation feature. v is the age value
+          const regex = /^(http|https?):\/\/+(www\.)?[.a-z0-9\s]{3,}\.[a-z]{2,3}(\/#?[.a-z0-9\s])?/;
+          return regex.test(value);
+        },
+        message: 'Must be a Valid URL', // when the validator returns false, this message will be displayed
+      },
+    },
     favoritesCount: { type: Number, default: 0 },
     comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
     tagList: [{ type: String }],
